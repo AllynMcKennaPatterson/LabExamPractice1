d53909e67aae88502fd2a42fef952400db8db524
Allyn <G00392765@atu.ie>
2022-12-06 18:32:48 +0000

    Created Business class' instance variables and getters

--- src/main/java/ie/atu/Business.java
+++ src/main/java/ie/atu/Business.java
@@ -1,4 +1,30 @@
-package ie.atu
+package ie.atu;
 
 public class Business {
+    private String name;
+    private long id;
+    private String address;
+    private int openTime;
+    private int closeTime;
+
+    public String getName() {
+        return name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public int getOpenTime() {
+        return openTime;
+    }
+
+    public int getCloseTime() {
+        return closeTime;
+    }
+
 }
--- src/test/java/ie/atu/BusinessSuccessTest.java
+++ src/test/java/ie/atu/BusinessSuccessTest.java
@@ -1,4 +1,4 @@
-package ie.atu
+package ie.atu;
 
 public class BusinessSuccessTest {
 }
